40eaba3868b4a4413631b8c444bf59c2a464ae87
MaiconSouzaDevFrontEnd <dev.front.end.maiconsouza@gmail.com>
2025-01-02 20:30:30 -0300

    modificado alguns css

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 const ulMenu =document.querySelectorAll('.ulMenu');
-const aLink = document.querySelectorAll('.a-link');
+const aLink =[...document.querySelectorAll('.a-link')];
 const divSubMenu = document.querySelectorAll('.divSubMenu')
 
 aLink.forEach((item) => {
@@ -8,9 +8,16 @@ aLink.forEach((item) => {
     function navFunction(e){
         e.stopPropagation();
         let event = e.target;
-        event.classList.toggle('show')
+        event.classList.toggle('show');
+        elInterno();
+    }
+
+    function elInterno(){
+      if()
     }
 });
+
+
 
 /* elementoSite.forEach((item) => {
   item.addEventListener('click', clicando);
