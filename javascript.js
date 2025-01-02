bf4b05b25c200d1ef8e840c24c4706bf97f98ddc
MaiconSouzaDevFrontEnd <dev.front.end.maiconsouza@gmail.com>
2025-01-02 19:16:38 -0300

    criando um click com js

--- javascript.js
+++ javascript.js
@@ -1,8 +1,22 @@
-const btnLink = document.querySelectorAll('ulMenuBtn-link');
+const ulMenu =document.querySelectorAll('.ulMenu');
+const aLink = document.querySelectorAll('.a-link');
+const divSubMenu = document.querySelectorAll('.divSubMenu')
 
+aLink.forEach((item) => {
+    item.addEventListener('click', navFunction);
 
-btnLink.forEach((btn) =>
-    btn.addEventListener('click', (event) => {
-     btnLink.classList.toggle('show')
-    })
-  );
+    function navFunction(e){
+        e.stopPropagation();
+        let event = e.target;
+        event.classList.toggle('show')
+    }
+});
+
+/* elementoSite.forEach((item) => {
+  item.addEventListener('click', clicando);
+
+  function clicando(e){
+    e.stopPropagation();
+    e.target.classList.toggle('show')
+  }
+}) */
